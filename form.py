97833f7e81a40ec4031d653fad8c804743cff51f
Bhavya <bhavya.hari2002@gmail.com>
2022-10-14 22:29:18 +0530

    first commit

---
+++ form.py
@@ -0,0 +1,58 @@
+from telnetlib import STATUS
+from tkinter import *
+import connection
+import mysql.connector 
+
+
+conn = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Bhavya@hari22",
+  database="login_form"
+) 
+if(conn):
+  print("connection",conn)
+ 
+def getval():
+
+ mycursor = conn.cursor()
+# sql = "INSERT INTO registration (username, phone_no , password, email) VALUES ('%s',' %s',' %s','%s')" %(name.get(),contact.get(),pwd.get(),email_val.get())
+ sql = mycursor.execute("INSERT INTO registration VALUES ('%s',' %s',' %s','%s')" %(name.get(),contact.get(),pwd.get(),email_val.get()))
+ print(sql)
+ #mycursor.commit()
+ mycursor.close()
+ STATUS.set("inserted")
+ conn.close()
+
+root1 = Tk()
+root1.geometry("500x300")
+name= StringVar()
+pwd= StringVar()
+STATUS=StringVar()
+email_val= StringVar()
+
+Label(root1, text="sign-in", font= " ar 15 bold").place(x=10, y=10)
+Label(root1,  text = "Username").place(x = 40,  y = 60)  
+
+Label(root1,  text = "Password").place(x = 40, y = 90)  
+Label(root1,  text = "",textvariable=STATUS).place(x = 40, y = 120)  
+ 
+#confirm_pwd = Label(root, text="confirm_password").place(x=40, y=120)
+
+Label(root1, text="email").place(x =40, y=120)
+ 
+
+
+
+#name = Entry(root, width = 30).place(x = 110, y = 60)
+
+Entry(root1, width = 30, textvariable=name).place(x = 110, y = 60)
+
+Entry(root1, width=30, textvariable=pwd).place(x=110,y=90)
+Entry(root1 ,width=30,textvariable=email_val).place(x=110,y=120)
+
+
+Button(root1,width=35, text = "Submit",command=getval).place(x=40, y=210)
+
+mainloop()
+
